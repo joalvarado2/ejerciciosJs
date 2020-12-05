5e3468be9ec89dea2cba9326e4d52c9e8ec01fba
Jonathan Alvarado <jonathanalva92@gmail.com>
2020-12-04 19:57:29 -0500

    callback y promises

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-// encontrar una letra o vacal en una frase
+// encontrar una letra o vocal en una frase
 /* var cadena = prompt("escribe una frase") 
 var indices = [];
 for(var i = 0; i < cadena.length; i++) {
@@ -7,7 +7,67 @@ for(var i = 0; i < cadena.length; i++) {
           }
 }
 document.write(`la frase contiene ${indices.length} letras O
-`)
- */
+`) */
 
- 
+// ##########################################################################################
+// ejercicio paises con CALLBACKS
+
+/* const paises = [];
+
+function nuevoPais(pais, callback) {
+    paises.push(pais);
+    console.log(`agregando ${pais}`)
+    callback();
+}
+
+function mostrarPaises() {
+  console.log(paises);
+}
+
+function iniciarCallbackHell() {
+  setTimeout(() => {
+    nuevoPais("alemania", mostrarPaises)
+    setTimeout(() => {
+      nuevoPais("Francia", mostrarPaises)
+      setTimeout(() => {
+        nuevoPais("Holanda", mostrarPaises)
+        setTimeout(() => {
+          nuevoPais("Croacia", mostrarPaises)
+          setTimeout(() => {
+            nuevoPais("Nueva Zelanda", mostrarPaises)
+          },2000)
+        },2000)
+      },2000)
+    },2000)
+  },2000)
+}
+iniciarCallbackHell(); */
+
+// ###############################################################################################
+// ejercicio paises con PROMISES
+
+/* const paises = [];
+
+const nuevoPais = (pais) => new Promise ((resolve) => {
+  setTimeout (() => {
+    paises.push(pais);
+    resolve(`agregando ${pais}`)
+  }, 3000);
+})
+
+nuevoPais("Alemania")
+  .then( resultado => {
+    console.log(resultado)
+    console.log(paises)
+    return nuevoPais("Holanda")
+  })
+  .then( resultado => {
+    console.log(resultado)
+    console.log(paises)
+    return nuevoPais("Nueva Zelanda")
+  })
+  .then( resultado => {
+    console.log(resultado)
+    console.log(paises)
+  })
+ */
