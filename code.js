13a3117afdce78728cdc8e1065139b1278649a9b
Jonathan Alvarado <jonathanalva92@gmail.com>
2020-12-04 19:01:19 -0500

    creando repositorio

---
+++ code.js
@@ -0,0 +1,13 @@
+// encontrar una letra o vacal en una frase
+/* var cadena = prompt("escribe una frase") 
+var indices = [];
+for(var i = 0; i < cadena.length; i++) {
+	if (cadena[i].toLowerCase() === "o") {
+        indices.push(i);
+          }
+}
+document.write(`la frase contiene ${indices.length} letras O
+`)
+ */
+
+ 
